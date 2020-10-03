92a73df8e72b03aafc9dde6e42f70c201c36406d
chkumar <himakiran@gmail.com>
2020-10-04 00:17:46 +0530

    Initialised PitchPerfect
    Initialised project dir.

---
+++ PitchPerfectCHK/PitchPerfectCHK/PitchPerfectCHKApp.swift
@@ -0,0 +1,17 @@
+//
+//  PitchPerfectCHKApp.swift
+//  PitchPerfectCHK
+//
+//  Created by Himakiran Kumar on 04/10/20.
+//
+
+import SwiftUI
+
+@main
+struct PitchPerfectCHKApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
