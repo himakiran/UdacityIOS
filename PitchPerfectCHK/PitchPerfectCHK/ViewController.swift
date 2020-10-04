8ba8c8d4e4c082acbdba566f5dc977436ee09329
chkumar <himakiran@gmail.com>
2020-10-04 14:18:02 +0530

    From code to UI element
    changing the text of teh label as the user presses a button. Error

---
+++ PitchPerfectCHK/PitchPerfectCHK/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  PitchPerfectCHK
+//
+//  Created by Himakiran Kumar on 04/10/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var recordLabel: UILabel!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func recordAudio(_ sender: Any) {
+        recordLabel.text = "Recording in progress"
+    }
+    
+    
+}
+
