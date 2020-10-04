9ddd2db39d87f01e5fcbfcdd0af66d551e20ca2f
chkumar <himakiran@gmail.com>
2020-10-04 14:43:21 +0530

    view controller error: resolved
    removed redundant storyboard connections to code by deleting the filled circles in the editor gutter

--- PitchPerfectCHK/PitchPerfectCHK/ViewController.swift
+++ PitchPerfectCHK/PitchPerfectCHK/ViewController.swift
@@ -7,18 +7,19 @@
 
 import UIKit
 
+
 class ViewController: UIViewController {
 
     @IBOutlet weak var recordLabel: UILabel!
-    override func viewDidLoad() {
+    
+override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
     @IBAction func recordAudio(_ sender: Any) {
-        recordLabel.text = "Recording in progress"
+        recordLabel.text="recording.."
     }
-    
-    
 }
-
+    
+    
