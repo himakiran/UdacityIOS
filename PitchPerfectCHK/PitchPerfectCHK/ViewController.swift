838dade18c0a03ef8e3e1336601e4e39f3b72394
chkumar <himakiran@gmail.com>
2020-10-07 01:04:41 +0530

    UI Screen 1 complete
    The first UI screeen with actions and outlets complete.

--- PitchPerfectCHK/PitchPerfectCHK/ViewController.swift
+++ PitchPerfectCHK/PitchPerfectCHK/ViewController.swift
@@ -9,11 +9,24 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var recordingLabel: UILabel!
+    @IBOutlet weak var recordButton: UIButton!
+    @IBOutlet weak var stopRecordButton: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        stopRecordButton.isEnabled=false
     }
 
-
+    @IBAction func recordAudio(_ sender: Any) {
+        recordingLabel.text="Recording in progress .."
+        stopRecordButton.isEnabled=true
+        recordButton.isEnabled=false
+    }
+    
+    @IBAction func stopRecording(_ sender: Any) {
+        recordButton.isEnabled=true
+        stopRecordButton.isEnabled=false
+        recordingLabel.text="Tap to Record"
+    }
 }
 
