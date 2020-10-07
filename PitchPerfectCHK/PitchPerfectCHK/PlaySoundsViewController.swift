19c4300acdf2d1403057e757db7adb997341b915
chkumar <himakiran@gmail.com>
2020-10-07 12:10:58 +0530

    ibactions for PlaySoundViewController buttons
    ibactions implemented for play and stop buttons in the stryboard file and the respective functions in the swift file

--- PitchPerfectCHK/PitchPerfectCHK/PlaySoundsViewController.swift
+++ PitchPerfectCHK/PitchPerfectCHK/PlaySoundsViewController.swift
@@ -6,9 +6,41 @@
 //
 
 import UIKit
+import AVFoundation
 
 class PlaySoundsViewController: UIViewController {
+    
+    // MARK: Outlets
+
+    @IBOutlet weak var snailButton: UIButton!
+    @IBOutlet weak var chipmunkButton: UIButton!
+    @IBOutlet weak var rabbitButton: UIButton!
+    @IBOutlet weak var vaderButton: UIButton!
+    @IBOutlet weak var echoButton: UIButton!
+    @IBOutlet weak var reverbButton: UIButton!
+    @IBOutlet weak var stopButton: UIButton!
+    
+    // MARK: Recorded Audio URL
     var recordedAudioURL: URL!
+    
+    var audioFile:AVAudioFile!
+    var audioEngine:AVAudioEngine!
+    var audioPlayerNode: AVAudioPlayerNode!
+    var stopTimer: Timer!
+
+    enum ButtonType: Int {
+        case slow = 0, fast, chipmunk, vader, echo, reverb
+    }
+    
+    // MARK: Actions
+
+    @IBAction func playSoundForButton(_ sender: UIButton) {
+        print("Play Sound Button Pressed")
+    }
+
+    @IBAction func stopButtonPressed(_ sender: AnyObject) {
+        print("Stop Audio Button Pressed")
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
