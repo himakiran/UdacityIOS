1b304b982b75d2f51b8ca60b0ff2b515c7093b86
chkumar <himakiran@gmail.com>
2020-10-21 22:48:18 +0530

    added additonal code to display another label
    the button cycles through five colors as the count is incremented

--- clickCounter/clickCounter/ViewController.swift
+++ clickCounter/clickCounter/ViewController.swift
@@ -11,6 +11,8 @@ class ViewController: UIViewController {
     
     var count: Int = 0
     var label: UILabel!
+    var label2: UILabel = UILabel()
+    var button: UIButton = UIButton()
     
     
 
@@ -20,16 +22,27 @@ class ViewController: UIViewController {
         // Adding a UI label directly from code
         let uilabel = UILabel()
         uilabel.frame = CGRect(x: 150, y: 150, width: 60, height: 60)
-        uilabel.text = "  0" // spaces to align with click button below
+        uilabel.text = "00" // spaces to align with click button below
+        uilabel.backgroundColor=UIColor.darkGray
         view.addSubview(uilabel)
         self.label = uilabel
         
+        // add another UI label
+        let uilabel2 = UILabel()
+        uilabel2.frame = CGRect(x: 150, y: 250, width: 60, height: 60)
+        uilabel2.text = "Color"
+        uilabel2.backgroundColor = UIColor.darkGray
+        view.addSubview(uilabel2)
+        self.label2 = uilabel2
+        
         // Adding a button directly from code
         let uibutton = UIButton()
-        uibutton.frame = CGRect(x: 150, y: 200, width: 60, height: 60)
+        uibutton.frame = CGRect(x: 150, y: 350, width: 60, height: 60)
         uibutton.setTitle("Click", for: .normal)
-        uibutton.setTitleColor(UIColor.brown, for: .normal)
+        uibutton.setTitleColor(UIColor.black, for: .normal)
+        uibutton.backgroundColor = UIColor.magenta
         view.addSubview(uibutton)
+        self.button = uibutton
         
         // adding action to the ui button
         uibutton.addTarget(self, action: #selector(ViewController.incrementCount), for: UIControl.Event.touchUpInside)
@@ -37,8 +50,13 @@ class ViewController: UIViewController {
     }
     
     @objc func incrementCount() {
+        let colorsList: Array = [UIColor.blue,UIColor.red,UIColor.green,UIColor.orange,UIColor.black]
+        let colorsLabels: Array = ["blue","red","green","orange","black"]
         self.count+=1
         self.label.text = "\(self.count)"
+        self.label2.text = colorsLabels[self.count % 5]
+        self.button.backgroundColor = colorsList[self.count % 5]
+        
     }
 
 
