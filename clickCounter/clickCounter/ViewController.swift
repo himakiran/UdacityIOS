0b3df28acf60e40a6d189c359ecacb25e25960e5
chkumar <himakiran@gmail.com>
2020-10-21 21:36:08 +0530

    Added UI elements to view directly from code
    As opossed to adding ui elements in story board and then connecting them to code, here we added them directly to code

--- clickCounter/clickCounter/ViewController.swift
+++ clickCounter/clickCounter/ViewController.swift
@@ -12,6 +12,20 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        // Adding a UI label directly from code
+        let uilabel = UILabel()
+        uilabel.frame = CGRect(x: 150, y: 150, width: 60, height: 60)
+        uilabel.text = "  0" // spaces to align with click button below
+        view.addSubview(uilabel)
+        
+        // Adding a button directly from code
+        let uibutton = UIButton()
+        uibutton.frame = CGRect(x: 150, y: 200, width: 60, height: 60)
+        uibutton.setTitle("Click", for: .normal)
+        uibutton.setTitleColor(UIColor.brown, for: .normal)
+        view.addSubview(uibutton)
+        
+        
     }
 
 
